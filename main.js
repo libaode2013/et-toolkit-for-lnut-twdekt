8f02b967a7349d4f5d576b4a0ff39d302f3daf99
Li Baode <libaode2013@163.com>
2026-05-23 15:43:46 +0800

    rebuild file structure

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-//这个文件由index.html包含
-document.write("<script language='javascript' src='js/util.js'></script>");
-document.write("<script language='javascript' src='js/ribbon.js'></script>");
-document.write("<script language='javascript' src='js/systemdemo.js'></script>");
+function getUrlPath() {
+    let e = document.location.toString();
+    return -1 != (e = decodeURI(e)).indexOf("/") && (e = e.substring(0, e.lastIndexOf("/"))), e;
+}
+
+function onAction(control) {
+    switch (control.Id) {
+        case "default-button":
+            // window.Application.ShowDialog(getUrlPath() + "/ui/dialog.html", "这是一个对话框网页", 400 * window.devicePixelRatio, 400 * window.devicePixelRatio, false);
+            alert("Hello, World!");
+            break;
+        default:
+            return true;
+    }
+}
+
+function getImage(control) {
+    switch (control.Id) {
+
+        default:
+            return "images/error-picture.svg";
+    }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import { functionsScanner, copyFile } from "wpsjs/vite_plugins"
+import { copyFile } from "wpsjs/vite_plugins"
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: './',
     plugins: [
-        copyFile({ src: 'manifest.xml', dest: 'manifest.xml', }),
-        copyFile({ src: 'js', dest: 'js', }),
+        copyFile({ src: 'dialog', dest: 'dialog', }),
         copyFile({ src: 'images', dest: 'images', }),
-        copyFile({ src: 'ui', dest: 'ui', }),
         copyFile({ src: 'main.js', dest: 'main.js', }),
         copyFile({ src: 'ribbon.xml', dest: 'ribbon.xml', }),
-        functionsScanner({
-            inputJsPath: 'js/functions.js',
-            outputJsonPath: 'functions.json',
-            namespace: 'HelloEt',
-        }),
     ],
     resolve: {
         alias: {
